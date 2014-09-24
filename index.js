61124e0878725b7e27683787a1e85cf0ea21a332
Jxck <block.rxckin.beats@gmail.com>
2014-09-25 01:34:03 +0900

    up

--- index.js
+++ index.js
@@ -38,10 +38,14 @@ LogStorage.prototype.each = function(fn) {
 };
 
 LogStorage.prototype.dump = function(level) {
+  if (level === 'All') level = undefined;
+
   var arr = [];
   this.each(function(k, value) {
+    if (level && value.level !== level) {
+      return;
+    }
     arr.push(value);
-    console.log(k, JSON.stringify(value));
   });
   return arr;
 };
@@ -77,6 +81,31 @@ LogStorage.prototype.error = function() {
   this.write(ERROR, arguments);
 };
 
+// upload
+LogStorage.prototype.pack = function(level) {
+  return this.dump(level)
+             .map(function(e) {
+               return [e.timestamp, e.level, e.message].join(' ');
+             }).join('\n');
+};
+
+LogStorage.prototype.upload = function(level, url) {
+  var boundary = '--logstrage';
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-Type', 'mutipart/form-data; boundary=' + boundary);
+  xhr.send([
+     boundary,
+     'Content-Disposition: form-data; name="file"; filename="blob"',
+     'Content-Type: text/plain',
+     '',
+     this.pack(level),
+     '',
+     boudary
+  ]);
+};
+
 
 // main
 
@@ -84,16 +113,13 @@ var appLogger = new LogStorage('app');
 
 appLogger.clear();
 appLogger.debug('b', 'd');
+appLogger.debug('b', 'd');
 
 var n = 0;
-while(n++ < 1000) {
+while(n++ < 10) {
   appLogger.trace('a', 1, 20);
 }
 appLogger.error('e');
 
-var arr = appLogger.dump(TRACE);
-arr.map(function(e) {
-  return [e.timestamp, e.level, e.message].join(' ');
-}).forEach(function(line) {
-  console.log(line);
-});
+var pack = appLogger.pack(DEBUG);
+console.log(pack);
