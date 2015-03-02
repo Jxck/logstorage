d3b08bb335caee6112921262f875afa161eb8a51
Jxck <block.rxckin.beats@gmail.com>
2015-03-02 14:47:13 +0900

    update sample

--- logstorage.js
+++ logstorage.js
@@ -133,22 +133,3 @@ Logger.prototype.trace = function() {
   var args = this.slice.call(arguments);
   this._write('TRACE', args);
 }
-
-var format = '[%date] %category %level (%file) - %message';
-var appLogger = Logger.getLogger('APP', {
-  loglevel: 'FATAL',
-  format: format,
-  storage: {
-    type: 'localStorage',
-    key: 'log',
-    limit: 1000 * 10 // 10K
-  }
-});
-
-var a = { hoge: 100 };
-appLogger.trace('the value of "hoge" at', a, 'is', 100);
-appLogger.debug('the value of "hoge" at', a, 'is', 100);
-appLogger.info( 'the value of "hoge" at', a, 'is', 100);
-appLogger.warn( 'the value of "hoge" at', a, 'is', 100);
-appLogger.error('the value of "hoge" at', a, 'is', 100);
-appLogger.fatal('the value of "hoge" at', a, 'is', 100);
---
+++ sample/sample.js
@@ -0,0 +1,18 @@
+var format = '[%date] %category %level (%file) - %message';
+var appLogger = Logger.getLogger('APP', {
+  loglevel: 'FATAL',
+  format: format,
+  storage: {
+    type: 'localStorage',
+    key: 'log',
+    limit: 1000 * 10 // 10K
+  }
+});
+
+var a = { hoge: 100 };
+appLogger.trace('the value of "hoge" at', a, 'is', 100);
+appLogger.debug('the value of "hoge" at', a, 'is', 100);
+appLogger.info( 'the value of "hoge" at', a, 'is', 100);
+appLogger.warn( 'the value of "hoge" at', a, 'is', 100);
+appLogger.error('the value of "hoge" at', a, 'is', 100);
+appLogger.fatal('the value of "hoge" at', a, 'is', 100);
